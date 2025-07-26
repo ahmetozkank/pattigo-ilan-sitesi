4854148dda0c23356605c38ac74631f270aa7348
Ahmet Özkan Kulaksız <148324756+ahmetozkank@users.noreply.github.com>
2025-07-26 10:25:55 +0300

    ayar.php

--- libs/ayar.php
+++ libs/ayar.php
@@ -1,16 +1,24 @@
 <?php
+    // PHP tarafı için saat dilimini Türkiye olarak ayarla
+    date_default_timezone_set('Europe/Istanbul');
 
-    $server ="localhost";
+    // Veritabanı bağlantı bilgileri
+    $server = "localhost";
     $username = "root";
     $password = "";
     $database = "ureticiden";
 
-
+    // MySQL bağlantısı oluştur
     $connection = mysqli_connect($server, $username, $password, $database);
-    mysqli_set_charset($connection, "UTF8");
-    if(mysqli_connect_errno() > 0) {
-        die("error: ".mysqli_connect_errno());
-    }
-   
 
+    // Karakter setini UTF-8 yap
+    mysqli_set_charset($connection, "UTF8");
+
+    // MySQL tarafında da saat dilimini Türkiye olarak ayarla
+    mysqli_query($connection, "SET time_zone = '+03:00'");
+
+    // Bağlantı hatasını kontrol et
+    if(mysqli_connect_errno() > 0) {
+        die("error: " . mysqli_connect_errno());
+    }
 ?>
